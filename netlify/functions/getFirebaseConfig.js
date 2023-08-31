9d4ca757aa4099281f3d89ebc9128f5b4a3d50e6
Engström <joel.engstrom@if.se>
2023-08-31 12:10:00 +0200

    autoommit

--- netlify/functions/getFirebaseConfig.js
+++ netlify/functions/getFirebaseConfig.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
   const firebaseConfig = process.env.FIREBASE_CONFIG;
-
+  console.log(firebaseConfig);
   return {
     statusCode: 200,
     body: firebaseConfig,
