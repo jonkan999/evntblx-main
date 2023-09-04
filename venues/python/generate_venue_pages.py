ab42e0c2023760c81eea860b62dc4f88c748e075
Engström <joel.engstrom@if.se>
2023-09-04 15:06:50 +0200

    autoommit

--- venues/python/generate_venue_pages.py
+++ venues/python/generate_venue_pages.py
@@ -14,14 +14,22 @@ template = env.get_template("venue_template.html")
 venues = read_all_from_venues()
 
 
+# Function to clean and format the venue name for the output filename
+def clean_filename(name):
+    name = name.replace('-', ' ').replace('å', 'a').replace('ä', 'a').replace('ö', 'o')
+    return '-'.join(name.split())
+
 # Generate HTML files for each venue
 for venue in venues:
     print("Generates:" + venue["venueInfo"]["name"] + ".html")
+    
     # Render the Jinja2 template with the data
     html_content = template.render(venue=venue)
 
     # Create a new HTML file
-    output_path = f"C:/Users/engjoe/festlokalerstockholm/venues/{venue['venueInfo']['name'].replace('-', ' ').replace(' ', '-').lower().replace('å', 'a').replace('ä', 'a').replace('ö', 'o')}.html"
+    cleaned_name = clean_filename(venue['venueInfo']['name'])
+    output_path = os.path.join("C:/Users/engjoe/festlokalerstockholm/venues", f"{cleaned_name}.html")
+    
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(output_path, "w", encoding="utf-8") as output_file:
         output_file.write(html_content)
