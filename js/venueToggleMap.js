d3135b4a69c8ea8c0d0b8db872cbe4797556ae14
Engström <joel.engstrom@if.se>
2023-09-07 21:36:19 +0200

    autocommit

--- js/venueToggleMap.js
+++ js/venueToggleMap.js
@@ -3,27 +3,31 @@ const toggleButtonMobile = document.getElementById("toggleMapButtonMobile");
 const venuesSection = document.querySelector(".venues-section");
 const mapSection = document.querySelector(".map-section");
 
+// Set an initial state
+let isMapClose = true;
+
 // Add a click event listener to the button
 toggleButtonMobile.addEventListener("click", function () {
-  if (venuesSection.style.display === "block") {
-    // If venues-section is currently displayed, switch to map
+  // Toggle between the sections
+  if (isMapClose) {
     venuesSection.style.display = "none";
-    mapSection.style.display = "block";
+    mapSection.style.width = "100%";
     toggleButtonMobile.innerHTML = `
       <div class="icon-container">
         <ion-icon name="list-outline"></ion-icon>
       </div>
       <p>lista</p>
     `;
+    isMapClose = false;
   } else {
-    // If map-section is currently displayed, switch to venues
     venuesSection.style.display = "block";
-    mapSection.style.display = "none";
+    mapSection.style.width = "0";
     toggleButtonMobile.innerHTML = `
       <div class="icon-container">
         <ion-icon name="map-outline"></ion-icon>
       </div>
       <p>karta</p>
     `;
+    isMapClose = true;
   }
 });
