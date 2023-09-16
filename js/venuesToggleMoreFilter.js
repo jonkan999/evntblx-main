fbef646cff6ce4d21716406e5df983186d425214
Engström <joel.engstrom@if.se>
2023-09-16 18:16:31 +0200

    autocommit

--- js/venuesToggleMoreFilter.js
+++ js/venuesToggleMoreFilter.js
@@ -32,7 +32,7 @@ function toggleFilters() {
    */
   if (isMobile) {
     filterSection.style.height = isFiltersVisible
-      ? "calc(5 * (4.5rem + 2.5rem) + var(--header-size)) - 4rem"
+      ? "calc(5 * (4.5rem + 2.5rem) + var(--header-size)) - 6.75rem"
       : "calc(1 * (4.5rem + 2.5rem) + var(--header-size))";
   } else {
     filterSection.style.height = isFiltersVisible
