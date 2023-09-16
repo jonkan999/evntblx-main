51c48eedd3159e770492a61a56ee409e35ebaa46
Engström <joel.engstrom@if.se>
2023-09-16 10:42:04 +0200

    autocommit

---
+++ venues/python/update_site.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Define the paths to Python executables and script files
+python_exe = r"C:\Users\engjoe\AppData\Local\Programs\Python\Python39\python.exe"
+generate_venue_boxes_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_venue_boxes.py"
+generate_venue_pages_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_venue_pages.py"
+
+# Run the Python scripts using subprocess
+subprocess.run([python_exe, generate_venue_boxes_script])
+subprocess.run([python_exe, generate_venue_pages_script])
+
+# Run Git commands
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "autocommit"])
+subprocess.run(["git", "push"])
