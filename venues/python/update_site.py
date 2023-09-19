5b1e2b18a2fb8c4e089d42df9c59bc6e923e945a
Engström <ENGJOEA@ifint.biz>
2023-09-19 21:26:54 +0200

    autocommit

--- venues/python/update_site.py
+++ venues/python/update_site.py
@@ -4,11 +4,12 @@ import subprocess
 python_exe = r"C:\Users\engjoe\AppData\Local\Programs\Python\Python39\python.exe"
 generate_venue_boxes_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_venue_boxes.py"
 generate_venue_pages_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_venue_pages.py"
-generate_venue_pages_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_seo_venue_box_page.py"
+generate_seo_venue_box_pages_script = r"c:\Users\engjoe\festlokalerstockholm\venues\python\generate_seo_venue_box_page.py"
 
 # Run the Python scripts using subprocess
 subprocess.run([python_exe, generate_venue_boxes_script])
 subprocess.run([python_exe, generate_venue_pages_script])
+subprocess.run([python_exe, generate_seo_venue_box_pages_script])
 
 # Run Git commands
 subprocess.run(["git", "add", "."])
