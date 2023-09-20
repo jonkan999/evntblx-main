e781ba79397fe9b3823de10aaea019709d25314a
Engström <ENGJOEA@ifint.biz>
2023-09-20 09:00:35 +0200

    added new disengage filter functionality for mobile

--- js/venueToggleMap.js
+++ js/venueToggleMap.js
@@ -3,8 +3,14 @@ const toggleButtonMobile = document.getElementById("toggleMapButtonMobile");
 const venuesSection = document.querySelector(".venues-section");
 const mapSection = document.querySelector(".map-section");
 
+//reset filter style on map open
+const filtersSection = document.querySelector(".filters-section");
+const header= document.querySelector(".header");
+ 
+
 // Set an initial state
 let isMapClose = true;
+const isMobile = window.innerWidth < 545;
 
 // Add a click event listener to the button
 toggleButtonMobile.addEventListener("click", function () {
@@ -18,6 +24,14 @@ toggleButtonMobile.addEventListener("click", function () {
       </div>
       <p>lista</p>
     `;
+    //reset filter style on map open
+    if (isMobile) {
+      filtersSection.style.opacity = "1";
+      filtersSection.style.marginTop = "var(--header-size)";
+      filtersSection.style.position = "fixed"
+      header.style.opacity = "1";
+      mapSection.style.paddingTop = "calc(1 * (4.5rem + 2.5rem) + 0.5rem + 2*var(--header-size))"
+    }
     isMapClose = false;
   } else {
     venuesSection.style.display = "block";
