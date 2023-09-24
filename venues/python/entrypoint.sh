b1ea7414582bb1914e2860a9417f76831832dd4d
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 17:23:39 +0200

    auto

--- venues/python/entrypoint.sh
+++ venues/python/entrypoint.sh
@@ -3,6 +3,13 @@
 # Change directory to the repository directory
 cd /app/repo
 
+# Set the Git user identity (replace with your actual email and name)
+git config --global user.email "joel.engstrom17@gmail.com"
+git config --global user.name "jonkan999"
+
+# Configure Git to use SSH (if not already configured)
+git config --global core.sshCommand "ssh -i /root/.ssh/id_rsa"
+
 # Pull from the Git repository to update it (in case you're running it manually)
 git pull
 
