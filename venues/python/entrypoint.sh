0cb1634fcccab070cf95aea3c7e9faf588cd8b0d
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 14:39:03 +0200

    auto

--- venues/python/entrypoint.sh
+++ venues/python/entrypoint.sh
@@ -3,19 +3,26 @@
 # Specify the repository URL
 repo_url="https://github.com/jonkan999/evntblx-main.git"
 
-# Ensure the /app directory exists
-mkdir -p /app
+# Specify the target directory where you want to clone the repository
+target_dir="/app"
 
-# Change directory to the /app directory
-cd /app
+# Ensure the target directory exists
+mkdir -p "$target_dir"
 
-# Initialize the Git repository
-git init
-git remote add origin "$repo_url"
+# Change directory to the target directory
+cd "$target_dir"
 
+# Check if the target directory is already a Git repository
+if [ -d .git ]; then
+    # If it's a Git repository, remove it to start fresh
+    rm -rf .git
+fi
 
-# Pull from the Git repository to update it
-git pull origin main
+# Clone the Git repository into the target directory
+git clone "$repo_url" "$target_dir"
+
+# Change directory to the cloned repository
+cd "$target_dir"
 
 # Run your Python script
 python "venues/python/generate_venue_boxes_docker.py"
