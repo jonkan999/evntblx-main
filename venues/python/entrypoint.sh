5e2fe11424d9c83106fdb8f29de2f4714bd82d61
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 18:49:48 +0200

    auto

--- venues/python/entrypoint.sh
+++ venues/python/entrypoint.sh
@@ -3,6 +3,19 @@
 # Change directory to the repository directory
 cd /app/repo
 
+# Read the PAT from the file
+PAT=$(cat /app/repo/github_token.txt)
+
+# Set the Git user identity (replace with your actual email and name)
+git config --global user.email "joel.engstrom17@gmail.com"
+git config --global user.name "jonkan999"
+
+# Set the Git PAT as an environment variable
+export GIT_PAT="$PAT"
+
+# Configure Git to use the PAT for authentication
+git config --global credential.helper '!f() { echo "password=$GIT_PAT"; }; f'
+
 # Pull from the Git repository to update it (in case you're running it manually)
 git pull
 
