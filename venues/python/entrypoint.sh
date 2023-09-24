0b4d457b16aa98dd99a3136c3c5244e0932ffc6b
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 14:22:26 +0200

    auto

--- venues/python/entrypoint.sh
+++ venues/python/entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Set the repository directory
-repo_dir="/app"
+# Specify the repository URL
+repo_url="https://github.com/jonkan999/evntblx-main.git"
 
-# Change directory to the repository directory
-cd "$repo_dir"
+# Ensure the /app directory exists
+mkdir -p /app
+
+# Change directory to the /app directory
+cd /app
+
+# Initialize the Git repository if it doesn't exist
+if [ ! -d .git ]; then
+    git init
+    git remote add origin "$repo_url"
+fi
 
 # Pull from the Git repository to update it
-git pull
+git pull origin main
 
 # Run your Python script
-python "$repo_dir/venues/python/generate_venue_boxes_docker.py"
+python "venues/python/generate_venue_boxes_docker.py"
