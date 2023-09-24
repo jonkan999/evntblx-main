7346f317f55567ea9bdcb13deeb072f9e6d7d95b
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 16:20:12 +0200

    auto

--- venues/python/entrypoint.sh
+++ venues/python/entrypoint.sh
@@ -3,6 +3,8 @@
 # Change directory to the repository directory
 cd /app/repo
 
+git config --global user.email "joel.engstrom17@gmail.com"
+git config --global user.name "jonkan999"
 git config --global credential.helper '!f() { echo "password=$GIT_PAT"; }; f'
 
 # Pull from the Git repository to update it (in case you're running it manually)
@@ -12,6 +14,6 @@ git pull
 python "venues/docker_scripts/update_site_docker.py"
 
 # Add, commit, and push changes made by the script
-git add .
+git add --all
 git commit -m "autocommit"
 git push
