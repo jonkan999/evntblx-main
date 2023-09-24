3eca235c35ba29cb7111fc2c860844c74fa3f0d4
Jonkan <joel.engstrom17@gmail.com>
2023-09-24 13:37:18 +0200

    auto

---
+++ venues/python/entrypoint.sh
@@ -0,0 +1,5 @@
+# Clone the Git repository every time the container is run
+git clone https://github.com/jonkan999/evntblx-main.git /app
+
+# Run your Python script
+python venues/python/generate_venue_boxes_docker.py
