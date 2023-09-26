0724feb317ce0b03067245b0a1c3de4a6e748b3c
Jonkan <joel.engstrom17@gmail.com>
2023-09-26 18:52:05 +0200

    auto

--- js/firebaseProduction.js
+++ js/firebaseProduction.js
@@ -74,7 +74,9 @@ fetch("/.netlify/functions/getFirebaseConfig")
         console.log("Venue data added to Firestore");
         // You can perform further actions or show a success message here
         // Update the document to set trigger_initial to true
-        const triggerInitialDocRef = doc(db, "venues", "trigger_initial");
+        const triggerInitialDocRef = db
+          .collection("venues")
+          .doc("trigger_initial");
         await updateDoc(triggerInitialDocRef, {
           trigger_initial: true,
         });
