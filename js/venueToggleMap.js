b9c1e92d694375928417528f8bdbd1e525c26f61
Jonkan <joel.engstrom17@gmail.com>
2023-10-02 21:08:37 +0200

    auto

--- js/venueToggleMap.js
+++ js/venueToggleMap.js
@@ -3,10 +3,6 @@ const toggleButtonMobile = document.getElementById("toggleMapButtonMobile");
 const venuesSection = document.querySelector(".venues-section");
 const mapSection = document.querySelector(".map-section");
 
-//reset filter style on map open
-const filtersSection = document.querySelector(".filters-section");
-const childElements = filtersSection.children;
-
 const header = document.querySelector(".header");
 
 // Set an initial state
@@ -28,13 +24,17 @@ toggleButtonMobile.addEventListener("click", function () {
     `;
     //reset filter style on map open
     if (isMobile) {
-      filtersSection.style.opacity = "1";
-      filtersSection.style.marginTop = "var(--header-size)";
-      filtersSection.style.position = "fixed";
-      for (let i = 0; i < childElements.length; i++) {
-        childElements[i].style.display = "grid";
+      //reset filter style on map open
+      const filtersSection = document.querySelector(".filters-section");
+      if (filtersSection) {
+        const childElements = filtersSection.children;
+        filtersSection.style.opacity = "1";
+        filtersSection.style.marginTop = "var(--header-size)";
+        filtersSection.style.position = "fixed";
+        for (let i = 0; i < childElements.length; i++) {
+          childElements[i].style.display = "grid";
+        }
       }
-
       header.style.opacity = "1";
       /*  mapSection.style.paddingTop = "calc(1 * (4.5rem + 2.5rem) + 0.5rem + 2*var(--header-size))" */
     }
